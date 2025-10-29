a88efadc71b4c5b3a344ce70f74176701272de4a
FELIX OMONDI ODHIAMBO <felixokiya10@gmail.com>
2025-10-29 13:49:46 +0300

    procofile the application.css

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -4,9 +4,8 @@
 //= link active_admin.js
 //= link application.css
 //= link controllers/application.js
- //= link controllers/hello_controller.js
-  //= link controllers/index.js
-
+//= link controllers/hello_controller.js
+//= link controllers/index.js
 //= link application.js
 
 
