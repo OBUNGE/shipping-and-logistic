00b8773b01692be30d1e0bbf28fcafc0a1c5361e
FELIX OMONDI ODHIAMBO <felixokiya10@gmail.com>
2025-10-30 21:41:40 +0300

    commit

--- config/application.rb
+++ config/application.rb
@@ -8,25 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Shiping
   class Application < Rails::Application
-
-    config.active_storage.variant_processor = :vips
-    config.active_storage.variant_processor = :mini_magick
-
-
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    # ✅ Use MiniMagick for ActiveStorage variants
+    config.active_storage.variant_processor = :mini_magick
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.autoload_lib(ignore: %w[assets tasks])
   end
 end
