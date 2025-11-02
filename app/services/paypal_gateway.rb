98cdf944734c17a12400bdb0170ad4bedc748ff6
FELIX OMONDI ODHIAMBO <felixokiya10@gmail.com>
2025-11-02 16:06:39 +0300

    pAYSTACK

--- app/services/paypal_gateway.rb
+++ app/services/paypal_gateway.rb
@@ -69,7 +69,7 @@ class PaypalGateway
 
   private
 
-  def callback_url(params = {})
-    paypal_callback_order_payments_url(@order, params)
-  end
+ def callback_url
+  paypal_callback_url(@order, host: ENV["APP_HOST"] || "shipping-and-logistic-wuo1.onrender.com")
+end
 end
