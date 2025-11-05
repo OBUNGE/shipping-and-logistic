9464886c52eb10a30baea7c1bffb634f041a2f5a
FELIX OMONDI ODHIAMBO <felixokiya10@gmail.com>
2025-11-05 15:32:08 +0300

    delivry system to order

--- db/migrate/20251017064045_add_delivery_fields_to_shipments.rb
+++ db/migrate/20251017064045_add_delivery_fields_to_shipments.rb
@@ -1,11 +1,14 @@
 class AddDeliveryFieldsToShipments < ActiveRecord::Migration[8.0]
   def change
-    add_column :shipments, :first_name, :string
-    add_column :shipments, :last_name, :string
-    add_column :shipments, :address, :text
-    add_column :shipments, :phone_number, :string
-    add_column :shipments, :country, :string
-    add_column :shipments, :city, :string
-    
+    add_column :shipments, :first_name,        :string
+    add_column :shipments, :last_name,         :string
+    add_column :shipments, :address,           :text
+    add_column :shipments, :phone_number,      :string
+    add_column :shipments, :alternate_contact, :string
+    add_column :shipments, :country,           :string
+    add_column :shipments, :city,              :string
+    add_column :shipments, :county,            :string
+    add_column :shipments, :region,            :string
+    add_column :shipments, :delivery_notes,    :text
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_05_120957) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_05_123050) do
   create_schema "auth"
   create_schema "extensions"
   create_schema "graphql"
@@ -255,6 +255,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_05_120957) do
     t.string "last_name"
     t.text "address"
     t.string "alternate_contact"
+    t.string "phone_number"
+    t.string "city"
+    t.string "county"
+    t.string "country"
+    t.string "region"
+    t.text "delivery_notes"
     t.index ["order_id"], name: "index_shipments_on_order_id"
   end
 
