3e239e3cf627a707fae298ff103b6748ec616197
FELIX OMONDI ODHIAMBO <felixokiya10@gmail.com>
2025-11-07 18:45:36 +0300

    added myaccount

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -3,7 +3,7 @@ class ShipmentsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_seller_or_admin, only: [:index]
   before_action :set_order, only: [:create, :show, :edit, :update, :track]
-  before_action :set_shipment, only: [:show, :edit, :update, :track]
+  before_action :set_order, only: [:new, :create, :show, :edit, :update, :track]
 
   require 'httparty'
 
