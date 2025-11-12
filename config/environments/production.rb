7213496ebd059a6ee0108e5ed6149b11be95b9b2
FELIX OMONDI ODHIAMBO <felixokiya10@gmail.com>
2025-11-12 14:09:28 +0300

    added myaccount

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,18 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  # ✅ ActionMailer SMTP settings for Gmail
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address:              "smtp.gmail.com",
+  port:                 587,
+  domain:               "gmail.com",
+  user_name:            ENV["GMAIL_USERNAME"],   # felixokiya10@gmail.com
+  password:             ENV["GMAIL_PASSWORD"],   # your Gmail App Password
+  authentication:       "plain",
+  enable_starttls_auto: true
+}
+
   # ✅ Use Supabase for Active Storage
   config.active_storage.service = :supabase
 
