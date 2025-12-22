7f8cbf90a8ba1156d83d2315a0a91f2d8bee44f3
FELIX OMONDI ODHIAMBO <felixokiya10@gmail.com>
2025-12-22 18:35:32 +0300

    latest commit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,8 +2,12 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find_by!(slug: params[:id])
     @products = @category.products.includes(:seller).order(created_at: :desc)
+
+    # ✅ Canonical URL for this category
+    @canonical_url = category_url(@category)
   end
-   def subcategories
+
+  def subcategories
     category = Category.find(params[:id])
     render json: category.subcategories.select(:id, :name)
   end
